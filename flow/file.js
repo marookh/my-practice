b3b178abc67c5be24684d5e8a45aea44dfffefc0
Mahsa <mahskh08@gmail.com>
2023-12-03 22:15:35 +0430

    practice switch statement

---
+++ flow/file.js
@@ -0,0 +1,23 @@
+let a = 8;
+
+if (a === 1) {
+    console.log('a is 1');
+} else if (a === 2) {
+    console.log('a is 2');
+} else if (a === 3) {
+    console.log('a is 3');
+} else if (a === 4) {
+    console.log('a is 4');
+} else if (a === 5) {
+    console.log('a is 5');
+} else if (a === 6) {
+    console.log('a is 6');
+} else if (a === 7) {
+    console.log('a is 7') 
+} else if (a === 8) {
+    console.log('a is 8')
+} else if (a === 9) {
+    console.log('a is 9');
+} else {
+    console.log('a is none of them')
+}
